4b0350070d83c3e6b63386d991d9f25500177815
Dylan Piergies <dylan.piergies@gmail.com>
2018-03-25 00:33:39 +0000

    Update to Spring Boot 2

--- contacts-backend-service/src/main/resources/db/migration/V1__create_tables.sql
+++ contacts-backend-service/src/main/resources/db/migration/V1__create_tables.sql
@@ -1,13 +1,15 @@
-CREATE TABLE "contact" (
-    "id"        SERIAL PRIMARY KEY,
+CREATE SCHEMA IF NOT EXISTS "contacts";
+
+CREATE TABLE "contacts"."contact" (
+    "id"        SERIAL       PRIMARY KEY,
     "title"     VARCHAR(31)  NULL,
     "forenames" VARCHAR(255) NULL,
     "surname"   VARCHAR(255) NULL
 );
 
-CREATE TABLE "telephone_number" (
-    "id"         SERIAL PRIMARY KEY,
+CREATE TABLE "contacts"."telephone_number" (
+    "id"         SERIAL       PRIMARY KEY,
     "number"     VARCHAR(255) NOT NULL,
     "type"       VARCHAR(31)  NULL,
-    "contact_id" INTEGER      NOT NULL REFERENCES "contact" ("id") ON DELETE CASCADE
+    "contact_id" INTEGER      NOT NULL REFERENCES "contacts"."contact" ("id") ON DELETE CASCADE
 );
